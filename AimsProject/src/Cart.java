f06381d590ed14f05729c887ea1c5449aca247a2
minhtruong171123 <133015047+phamtruong171123@users.noreply.github.com>
2023-11-13 21:20:08 +0700

    3.6
    bổ sung hàm in và tìm kiếm

--- AimsProject/src/Cart.java
+++ AimsProject/src/Cart.java
@@ -20,6 +20,24 @@ public class Cart {
         }
     }
 
+    public void addDigitalVideoDisc(DigitalVideoDisc... dvdList) {
+        for (DigitalVideoDisc dvd : dvdList) {
+            if (amount == MAX_NUMBERS_ORDERED) {
+                System.out.println("Giỏ hàng đã đầy, không thể thêm");
+                return;
+            }
+
+            for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+                if (itemsOrdered[i] == null) {
+                    itemsOrdered[i] = dvd;
+                    amount++;
+                    System.out.println("Đĩa " + dvd.getTitle() + " đã được thêm");
+                    break; // Chuyển đến DVD tiếp theo
+                }
+            }
+        }
+    }
+
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
             if (itemsOrdered[i] != null && itemsOrdered[i].equals(disc)) {
@@ -46,16 +64,59 @@ public class Cart {
         return total;
     }
     
+    
+    public void printCart() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+
+        for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+            if (itemsOrdered[i] != null) {
+                System.out.printf("%d. %s\n", (i + 1), itemsOrdered[i].toString());
+            }
+        }
+
+        System.out.println("Total cost: " + totalCost() + " $");
+        System.out.println("***************************************************");
+    }
+
+    public void Search(int id) {
+        for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+            if (itemsOrdered[i] != null && itemsOrdered[i].getID() == id) { // nếu thấy
+                System.out.print("TÌm thấy thành công: ");
+                System.out.println(itemsOrdered[i].toString()); // in ra thông tin đĩa
+                return;
+            }
+        }
+        System.out.println("Không tìm thấy đĩa");
+        
+    }
+    public void Search(String title){
+        for (int i = 0; i < MAX_NUMBERS_ORDERED; i++) {
+            if (itemsOrdered[i] != null && itemsOrdered[i].getTitle().equals(title)) {
+                System.out.print("TÌm thấy thành công :");
+               System.out.println(itemsOrdered[i].toString()); //in ra thông tin đĩa
+               return;
+            }
+        }
+    System.out.println("Không tìm thấy đĩa");
+    }
+    
+   
+    
+    
 
     public static void main(String[] args) {
         Cart anOrder = new Cart();
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 24.95f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Animation", "Aladin", 18.99f);
         anOrder.addDigitalVideoDisc(dvd3); // thêm dvd1
         anOrder.addDigitalVideoDisc(dvd1); //them dvd2
         anOrder.addDigitalVideoDisc(dvd2); //thêm dvd3
-        anOrder.removeDigitalVideoDisc(dvd2); // xóa đĩa 2
+        //anOrder.removeDigitalVideoDisc(dvd2); // xóa đĩa 2
         System.out.println("Total cost is: " + anOrder.totalCost()); //in ra tổng tiền trong giỏ hàng;
+        anOrder.printCart();
+
+        anOrder.Search("Aladin");
     }
 }
